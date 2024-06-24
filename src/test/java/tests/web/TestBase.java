c60ed5f324f86cb71a932cb923e156c2eeda9238
KirillTheMagnifico <96779884+KirillTheMagnifico@users.noreply.github.com>
2024-06-24 15:49:58 +0300

    Update TestBase.java
    Signed-off-by: KirillTheMagnifico <96779884+KirillTheMagnifico@users.noreply.github.com>

--- src/test/java/tests/web/TestBase.java
+++ src/test/java/tests/web/TestBase.java
@@ -1,1 +1,42 @@
+package web;
+
+import com.codeborne.selenide.Configuration;
+import io.restassured.RestAssured;
+import ru.rendezvous.config.App;
+import ru.rendezvous.helpers.AllureAttachments;
+import ru.rendezvous.helpers.DriverSettings;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.junit5.AllureJunit5;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static ru.rendezvous.helpers.UsersSetUp.usersSetUp;
+
+
+@ExtendWith({AllureJunit5.class})
+public class TestBase {
+
+    @BeforeAll
+    static void setUp() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        DriverSettings.configure();
+        //Configuration.startMaximized = false;
+    }
+
+    @BeforeAll
+    static void configureBaseUrl() {
+        RestAssured.baseURI = App.config.apiUrl();
+        Configuration.baseUrl = App.config.webUrl();
+        //usersSetUp();
+    }
+
+    @AfterEach
+    void addAttachments() {
+        AllureAttachments.screenshotAs("Last screenshot");
+        AllureAttachments.pageSource();
+        AllureAttachments.browserConsoleLogs();
+    }
+}
 
