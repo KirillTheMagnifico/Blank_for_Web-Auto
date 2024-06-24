a1ff3edb0cb84158edb36a90f5934787be37c940
KirillTheMagnifico <96779884+KirillTheMagnifico@users.noreply.github.com>
2024-06-24 16:06:12 +0300

    Create Project.java
    Signed-off-by: KirillTheMagnifico <96779884+KirillTheMagnifico@users.noreply.github.com>

---
+++ src/test/java/config/Project.java
@@ -0,0 +1,26 @@
+package config;
+
+import org.aeonbits.owner.ConfigFactory;
+
+public class Project {
+    public static ProjectConfig config = ConfigFactory.create(ProjectConfig.class, System.getProperties());
+
+    public static boolean isWebMobile() {//
+        int horizontalSize = 0;
+           try {
+               horizontalSize = Integer.parseInt(config.browserSize().split("x")[0]);
+            }
+            catch (NumberFormatException ex) {
+                ex.printStackTrace();
+            }
+        return horizontalSize <= 980;
+    }
+
+    public static boolean isRemoteWebDriver() {
+        return !config.remoteDriverUrl().equals("");
+    }
+
+    public static boolean isVideoOn() {
+        return !config.videoStorage().equals("");
+    }
+}
